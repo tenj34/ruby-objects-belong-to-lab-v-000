45ed1be707fa5bf888ea36bebdc328f688ba6b72
Juan Tenezaca <tenj34@gmail.com>
2018-08-08 15:37:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,3 @@
 class Song
-  attr_accessor :name, :artist
- @@all = []
-   def initialize(name)
-    @name = name
-    @@all << self
-  end
-   def self.all
-      @song = song
-    end
-   def artist_name
-    if @artist == nil
-      return nil
-    else
-      @artist.name
-    end
-  end
+  attr_accessor :song, :artist
 end
